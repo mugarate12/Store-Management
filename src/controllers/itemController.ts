d94dca1a02ca15849b535511b4ab78e01cb68870
unknown <serjumano17@gmail.com>
2020-04-09 22:09:15 -0300

    CRUD do item feito

--- src/controllers/itemController.ts
+++ src/controllers/itemController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express'
-import { getConnection } from 'typeorm'
+import { getConnection, getRepository } from 'typeorm'
 import { Item } from './../database/entity/Item'
 import { handleError } from './../utils/utils'
 
 export class ItemController {
-  public async create (req: Request, res: Response): Promise<void> {
+  public async create (req: Request, res: Response): Promise<Response> {
     const { marca, codigo, produto, quantidade, tipo, observacao } = req.body
 
     const item = new Item()
@@ -17,6 +17,66 @@ export class ItemController {
 
     const itemSaved = await getConnection().manager.save(item)
       .catch(error => handleError(error, res))
-    res.json(itemSaved)
+
+    return res.status(200).json(itemSaved)
+  }
+
+  public async index (req: Request, res: Response): Promise<Response> {
+    const itemRespository = getRepository(Item)
+    const itens = await itemRespository.find()
+
+    return res.status(200).json(itens)
+  }
+
+  public async indexById (req: Request, res: Response): Promise<Response | undefined> {
+    const id = req.params.id
+
+    const itemRespository = getRepository(Item)
+    const item = await itemRespository.findOne(id)
+
+    if (!item) {
+      return res.status(406).json({
+        error: `item com id ${id} não encontrado`
+      })
+    } else {
+      return res.status(200).json(item)
+    }
+  }
+
+  public async update (req: Request, res: Response): Promise<Response | undefined> {
+    const id = req.params.id
+    const { quantidade } = req.body
+
+    const itemRespository = getRepository(Item)
+    const itemToUpdate = await itemRespository.findOne(id)
+
+    // se não encontrei o item enviado no params, retorno o erro
+    if (!itemToUpdate) {
+      return res.status(406).json({
+        error: `item com o id ${id} não encontrado`
+      })
+    } else {
+      itemToUpdate.quantidade = quantidade
+
+      return await itemRespository.save(itemToUpdate)
+        .then((item: Item) => res.status(200).json(item))
+        .catch((error: Error) => handleError(error, res))
+    }
+  }
+
+  public async remove (req: Request, res: Response): Promise<Response | undefined> {
+    const id = req.params.id
+
+    const itemRespository = getRepository(Item)
+    const itemToRemove = await itemRespository.findOne(id)
+
+    if (!itemToRemove) {
+      return res.status(406).json({
+        error: `item com o id ${id} não encontrado`
+      })
+    } else {
+      await itemRespository.remove(itemToRemove)
+      return res.status(200).json({})
+    }
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,10 @@ import { ItemController } from './controllers/itemController'
 const routes = Router()
 
 // declaro minhas rotas
+routes.get('/item', new ItemController().index)
+routes.get('/item/:id', new ItemController().indexById)
 routes.post('/item', new ItemController().create)
+routes.put('/item/:id', new ItemController().update)
+routes.delete('/item/:id', new ItemController().remove)
 
 export default routes
